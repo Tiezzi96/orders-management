c1c7e062cca367238a646d6c02967a136a6e3c43
Bernardo <bernardo.tiezzi@edu.unifi.it>
2025-06-18 17:48:00 +0200

    Add Junit Test Case for Order Model Class

--- orders-management/src/main/java/com/unifi/ordersmgmt/model/Order.java
+++ orders-management/src/main/java/com/unifi/ordersmgmt/model/Order.java
@@ -1,9 +1,11 @@
 package com.unifi.ordersmgmt.model;
 
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.Objects;
 
 public class Order extends BaseElement {
-	
+
 	private Date date;
 	private double price;
 	private Client client;
@@ -42,11 +44,55 @@ public class Order extends BaseElement {
 		this.price = price;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Order{id='" + identifier + "', client=" + client.getIdentifier() + ", date=" + date + ", price=" + price
 				+ "}";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		if (this == obj)
+			return true;
+		Order otherOrder = (Order) obj;
+		if (this.identifier == null) {
+			if (otherOrder.identifier != null)
+				return false;
+		} else if (!this.identifier.equals(otherOrder.identifier)) {
+			return false;
+		} else if (!this.client.equals(otherOrder.getClient())) {
+			System.out.println("different client ");
+			return false;
+		} else if (!this.date.toInstant().truncatedTo(ChronoUnit.SECONDS)
+				.equals(otherOrder.getDate().toInstant().truncatedTo(ChronoUnit.SECONDS))) {
+			System.out.println("different date " + date + "   " + otherOrder.getDate());
+
+			return false;
+		} else if (this.price != otherOrder.getPrice()) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int idHash = (identifier != null) ? identifier.hashCode() : 0;
+		int clientHash = (client != null) ? client.hashCode() : 0;
+
+		int dateHash;
+		if (date != null) {
+			dateHash = date.toInstant().truncatedTo(ChronoUnit.SECONDS).hashCode();
+		} else {
+			dateHash = 0;
+		}
+
+		int priceHash = Double.valueOf(price).hashCode();
+
+		return Objects.hash(idHash, clientHash, dateHash, priceHash);
 	}
 
 }
