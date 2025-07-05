160a2e0f622e807e475418d4615d61fb508057d0
Bernardo <bernardo.tiezzi@edu.unifi.it>
2025-07-05 17:01:50 +0200

    Branch Repository: added Client and Order Repository Interface

---
+++ orders-management/src/main/java/com/unifi/ordersmgmt/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package com.unifi.ordersmgmt.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import com.unifi.ordersmgmt.model.Client;
+import com.unifi.ordersmgmt.model.Order;
+
+public interface OrderRepository extends Repository<Order> {
+	public List<Order> findOrderByYear(int year);
+
+	public List<Integer> getYearsOfOrders();
+
+	public List<Order> removeOrdersByClient(String clientId);
+
+	public Order updateOrder(String orderID, Map<String, Object> updates);
+
+	public List<Order> findOrdersByClientAndYear(Client client, int year);
+}
