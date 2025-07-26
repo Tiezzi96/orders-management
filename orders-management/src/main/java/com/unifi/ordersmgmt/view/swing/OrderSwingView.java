1d9d37f92d0c76acb25858c554c2a1bb751f94ec
Bernardo <bernardo.tiezzi@edu.unifi.it>
2025-07-26 12:10:56 +0200

    Branch OrderSwingView:
    Implement first version for OrderSwingView GUI with its initialize UI
    test. Add panel for clients' management.

--- orders-management/src/main/java/com/unifi/ordersmgmt/view/swing/OrderSwingView.java
+++ orders-management/src/main/java/com/unifi/ordersmgmt/view/swing/OrderSwingView.java
@@ -1,83 +1,262 @@
 package com.unifi.ordersmgmt.view.swing;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.util.List;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.SoftBevelBorder;
 
+import com.unifi.ordersmgmt.controller.OrderController;
 import com.unifi.ordersmgmt.model.Client;
 import com.unifi.ordersmgmt.model.Order;
 import com.unifi.ordersmgmt.view.OrderView;
 
+public class OrderSwingView extends JFrame implements OrderView {
 
-public class OrderSwingView extends JFrame implements OrderView  {
+	private JPanel contentPane;
+	private String FONT_TEXT = "Segoe UI";
+	private DefaultListModel clientListModel;
+	private JList listClients;
+	private JButton btnRemoveClient;
+	private JTextPane paneClientError;
+	private JLabel lblNewClient;
+	private JTextField textFieldNewClient;
+	private JLabel lblClientName;
+	private JButton btnNewClient;
+	private OrderController controller;
+	private JLabel lblrevenue;
 
 	public OrderSwingView() {
 		// TODO Auto-generated constructor stub
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(1000, 850));
+		setBounds(100, 100, 1000, 850);
+		setTitle("Order Management View");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		// racchiude l'intero pannello cliente
+		JPanel panel_clientManagement = new JPanel();
+		panel_clientManagement.setBackground(new Color(1, 1, 1));
+		panel_clientManagement.setBounds(0, 52, 1000, 304);
+		contentPane.add(panel_clientManagement);
+		panel_clientManagement.setLayout(null);
+
+		JPanel panel_clientTitle = new JPanel();
+		panel_clientTitle.setForeground(Color.WHITE);
+		panel_clientTitle.setBounds(360, 0, 640, 58);
+		panel_clientTitle.setBackground(new Color(24, 103, 64));
+	
+		panel_clientManagement.add(panel_clientTitle);
+		panel_clientTitle.setLayout(null);
+
+		JLabel lblClientsTitle = new JLabel("CLIENTI");
+		lblClientsTitle.setFont(new Font(FONT_TEXT, Font.BOLD, 17));
+		lblClientsTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblClientsTitle.setBounds(0, 0, 640, 58);
+		lblClientsTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		lblClientsTitle.setForeground(Color.WHITE);
+		panel_clientTitle.add(lblClientsTitle);
+
+		JScrollPane scrollPaneClientsList = new JScrollPane();
+		scrollPaneClientsList.setBorder(null);
+		scrollPaneClientsList.setBounds(360, 58, 640, 215);
+		panel_clientManagement.add(scrollPaneClientsList);
+
+		clientListModel = new DefaultListModel<>();
+		// clientListModel.addElement(new Client("CLIENT-00001", "Marta").toString());
+		// clientListModel.addElement(new Client("2", "Martino").toString());
+		listClients = new JList<>(clientListModel);
+		listClients.setFont(new Font(FONT_TEXT, Font.BOLD, 16));
+		listClients.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		listClients.setBorder(null);
+		listClients.setName("clientsList");
+		listClients.setSelectionBackground(Color.LIGHT_GRAY);
+		listClients.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listClients.setBackground(new Color(207, 234, 217)); // #CFEAD9
+		listClients.setFixedCellHeight(35);
+		scrollPaneClientsList.setViewportView(listClients);
+
+		btnRemoveClient = new JButton("Rimuovi cliente");
+		btnRemoveClient.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnRemoveClient.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		btnRemoveClient.setEnabled(false);
+		btnRemoveClient.setFont(new Font(FONT_TEXT, Font.BOLD, 14));
+		btnRemoveClient.setForeground(Color.WHITE);
+		btnRemoveClient.setBounds(673, 274, 312, 29);
+		panel_clientManagement.add(btnRemoveClient);
+
+		paneClientError = new JTextPane();
+		paneClientError.setText("");
+		paneClientError.setEditable(false);
+		paneClientError.setName("panelClientErrorMessage");
+		paneClientError.setForeground(new Color(255, 102, 102));
+		paneClientError.setBackground(new Color(15, 81, 50)); // #0F5132 verde scuro
+		paneClientError.setBounds(10, 5, 340, 43);
+		panel_clientManagement.add(paneClientError);
+
+		JPanel panel_newClient = new JPanel();
+		panel_newClient.setBounds(0, 58, 360, 243);
+		panel_newClient.setBackground(new Color(15, 81, 50)); // #0F5132 verde scuro
+		panel_clientManagement.add(panel_newClient);
+		panel_newClient.setLayout(null);
+
+		lblNewClient = new JLabel("NUOVO CLIENTE");
+		lblNewClient.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewClient.setBounds(6, 18, 350, 17);
+		lblNewClient.setFont(new Font(FONT_TEXT, Font.PLAIN, 18));
+		lblNewClient.setForeground(Color.WHITE);
+		panel_newClient.add(lblNewClient);
+
+		// riga per inserire id
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.WHITE);
+		separator.setBounds(133, 78, 180, 17);
+		panel_newClient.add(separator);
+
+		textFieldNewClient = new JTextField();
+		textFieldNewClient.setFont(new Font(FONT_TEXT, Font.BOLD, 13));
+		textFieldNewClient.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldNewClient.setBackground(Color.WHITE);
+		textFieldNewClient.setName("textField_clientName");
+		textFieldNewClient.setBorder(null);
+		textFieldNewClient.setBounds(133, 60, 180, 16);
+		textFieldNewClient.setBackground(new Color(15, 81, 50)); // #0F5132 verde scuro
+		textFieldNewClient.setCaretColor(Color.WHITE);
+		textFieldNewClient.setForeground(Color.WHITE);
+		textFieldNewClient.setColumns(10);
+		panel_newClient.add(textFieldNewClient);
+
+		lblClientName = new JLabel("Identificativo");
+		lblClientName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblClientName.setFont(new Font(FONT_TEXT, Font.PLAIN, 14));
+		lblClientName.setForeground(Color.WHITE);
+		lblClientName.setBounds(6, 60, 130, 25);
+		panel_newClient.add(lblClientName);
+
+		btnNewClient = new JButton("Aggiungi cliente");
+		btnNewClient.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewClient.setEnabled(false);
+		btnNewClient.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		btnNewClient.setForeground(Color.WHITE);
+		btnNewClient.setBounds(12, 98, 286, 29);
+		btnNewClient.setFont(new Font(FONT_TEXT, Font.BOLD, 14));
+		btnNewClient.setBounds(360, 274, 312, 29);
+		btnNewClient.setName("btnAddClient");
+		panel_clientManagement.add(btnNewClient);
+		
+		JPanel panel_orderManagement = new JPanel();
+		panel_orderManagement.setBackground(Color.WHITE);
+		panel_orderManagement.setBounds(0, 360, 1000, 646);
+		contentPane.add(panel_orderManagement);
+		panel_orderManagement.setLayout(null);
+
+		JPanel panel_revenueLabel = new JPanel();
+		panel_revenueLabel.setBorder(null);
+		panel_revenueLabel.setBounds(0, 5, 360, 88);
+		panel_revenueLabel.setBackground(new Color(245, 245, 245));
+		panel_orderManagement.add(panel_revenueLabel);
+		panel_revenueLabel.setLayout(null);
+
+		lblrevenue = new JLabel();
+		lblrevenue.setText(" ");
+		lblrevenue.setFont(new Font(FONT_TEXT, Font.PLAIN, 14));
+		lblrevenue.setBounds(2, 0, 355, 88);
+		lblrevenue.setForeground(new Color(89, 89, 89));
+		lblrevenue.setHorizontalAlignment(SwingConstants.CENTER);
+		lblrevenue.setName("revenueLabel");
+		panel_revenueLabel.add(lblrevenue);
+		
 	}
+
 	@Override
 	public void showAllClients(List<Client> clients) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setYearsOrders(List<Integer> yearsOfOrders) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void showAllOrders(List<Order> orders) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void showErrorClient(String message, Client client) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientRemoved(Client clientRemoved) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void clientAdded(Client clientAdded) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void orderAdded(Order orderAdded) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removeOrdersByClient(Client client) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void showOrderError(String message, Order order) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void orderRemoved(Order orderRemoved) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void orderUpdated(Order orderModified) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public void setOrderController(OrderController controller) {
+		// TODO Auto-generated method stub
+		this.controller = controller;
 	}
 
 }
--- orders-management/src/test/java/com/unifi/ordersmgmt/view/swing/OrderSwingViewTest.java
+++ orders-management/src/test/java/com/unifi/ordersmgmt/view/swing/OrderSwingViewTest.java
@@ -2,25 +2,73 @@ package com.unifi.ordersmgmt.view.swing;
 
 import static org.junit.Assert.*;
 
-import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
-import org.junit.Test;
+import java.util.regex.Pattern;
 
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.core.matcher.JLabelMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.assertj.swing.timing.Pause;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+
+import com.unifi.ordersmgmt.controller.OrderController;
+
+@RunWith(GUITestRunner.class)
 public class OrderSwingViewTest extends AssertJSwingJUnitTestCase {
 
+	private AutoCloseable autoCloseable;
+	private OrderSwingView orderSwingView;
+	private OrderController orderController;
+	private FrameFixture window;
+
 	@Override
-	protected void onSetUp() throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void onSetUp() throws Exception {
+
+		autoCloseable = MockitoAnnotations.openMocks(this);
+		GuiActionRunner.execute(() -> {
+			orderSwingView = new OrderSwingView();
+			orderSwingView.setOrderController(orderController);
+			return orderSwingView;
+		});
+
+		window = new FrameFixture(robot(), orderSwingView);
+		window.show();
+	    // serve per consentire al OS di dare la priorità alla finestra 
+		GuiActionRunner.execute(() -> {
+			orderSwingView.setAlwaysOnTop(true); 
+			orderSwingView.toFront(); 
+			orderSwingView.requestFocus(); 
+		});
+
+		Pause.pause(300); 
+
+		// Rimuove alwaysOnTop dopo il focus
+		GuiActionRunner.execute(() -> orderSwingView.setAlwaysOnTop(false));
 	}
-	
+
 	@Override
 	protected void onTearDown() throws Exception {
-		// TODO Auto-generated method stub
+		window.cleanUp();
+		autoCloseable.close();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitializeOrderView() {
+		window.label(JLabelMatcher.withText("CLIENTI"));
+		window.list("clientsList");
+		window.label(JLabelMatcher.withText("NUOVO CLIENTE"));
+		window.label(JLabelMatcher.withText("Identificativo"));
+		window.textBox("panelClientErrorMessage").requireText("");
+		window.textBox("panelClientErrorMessage").requireNotEditable();
+		window.textBox("textField_clientName").requireEnabled();
+		window.button(JButtonMatcher.withText("Aggiungi cliente")).requireDisabled();
+		window.button(JButtonMatcher.withText("Rimuovi cliente")).requireDisabled();
+		window.label(JLabelMatcher.withName("revenueLabel"));
+
 	}
 
 }
