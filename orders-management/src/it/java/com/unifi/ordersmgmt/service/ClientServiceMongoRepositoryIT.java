9ae850b2a68b1f75710a498d7318e56e13385dd4
Bernardo <bernardo.tiezzi@edu.unifi.it>
2025-08-06 19:25:30 +0200

    Branch test/integration-client-service:
    Configure the POM by adding the necessary plugins for integration
    testing: maven-failsafe-plugin, build-helper-maven-plugin, and
    docker-maven-plugin. Create the source folders src/it/java and
    src/it/resources. Create the class ClientServiceMongoRepositoryIT with a
    test method testAddClient to test MongoDB integration. Add the
    init_replicaSet.sh script to enable replica set support in the MongoDB
    Docker container for transaction compatibility.
    
    Progress on https://github.com/Tiezzi96/orders-management/issues/15

---
+++ orders-management/src/it/java/com/unifi/ordersmgmt/service/ClientServiceMongoRepositoryIT.java
@@ -0,0 +1,60 @@
+package com.unifi.ordersmgmt.service;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.unifi.ordersmgmt.model.Client;
+import com.unifi.ordersmgmt.model.Order;
+import com.unifi.ordersmgmt.repository.mongo.ClientMongoRepository;
+import com.unifi.ordersmgmt.repository.mongo.ClientSequenceGenerator;
+import com.unifi.ordersmgmt.repository.mongo.OrderMongoRepository;
+import com.unifi.ordersmgmt.repository.mongo.OrderSequenceGenerator;
+import com.unifi.ordersmgmt.transaction.*;
+import com.unifi.ordersmgmt.transaction.mongo.MongoTransactionManager;
+
+
+public class ClientServiceMongoRepositoryIT {
+	private MongoClient mongoclient;
+	private ClientService clientService;
+	private ClientMongoRepository clientRepository;
+	private OrderMongoRepository orderRepository;
+
+	@Before
+	public void setUp() {
+		mongoclient = MongoClients.create("mongodb://localhost:27017/?replicaSet=rs0");
+		TransactionManager transactionManager = new MongoTransactionManager(mongoclient, "client", "order", "budget");
+		clientService = new TransactionalClientService(transactionManager);
+		ClientSession session = mongoclient.startSession();
+		ClientSequenceGenerator clientSeqGen = new ClientSequenceGenerator(mongoclient, "budget");
+		clientRepository = new ClientMongoRepository(mongoclient, session, "budget", "client", clientSeqGen);
+		OrderSequenceGenerator seqGen = new OrderSequenceGenerator(mongoclient, "budget");
+		orderRepository = new OrderMongoRepository(mongoclient, session, "budget", "order", clientRepository, seqGen);
+		for (Client client : clientRepository.findAll()) {
+			clientRepository.delete(client.getIdentifier());
+		}
+		for (Order order : orderRepository.findAll()) {
+			orderRepository.delete(order.getIdentifier());
+		}
+
+	}
+
+	@After
+	public void tearDown() {
+		mongoclient.close();
+	}
+	
+	@Test
+	public void testAddClient() {
+		Client newClient = new Client("1", "test id");
+		clientService.saveClient(newClient);
+		assertThat(clientRepository.findAll()).containsExactly(newClient);
+	}
+
+}
